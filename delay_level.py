883617f248bfe02fe8a7dda41d2057868633e1cc
Aldandi Fitra Ramadhan <75534196+aldandifr@users.noreply.github.com>
2023-04-19 03:02:15 +0700

    Delete delay_level.py

--- delay_level.py
+++
@@ -1,9 +0,0 @@
-def delay_level(delay_time):
-    if delay_time == 0:
-        return "no delay"
-    elif delay_time <= 13:
-        return "little delay"
-    elif delay_time <= 15:
-        return "delayed"
-    else:
-        return "very delayed"
